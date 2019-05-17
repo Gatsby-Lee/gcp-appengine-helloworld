0466ef5c67dceadbd990b9dc05b186c234f28310
Gatsby Lee <thewayofthinking@gmail.com>
2019-05-17 09:43:22 -0700

    add appengine deployment

--- flask_standard/main.py
+++ flask_standard/main.py
@@ -1,3 +1,5 @@
+import flask
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask - GoogleAppEngine(GAE) Standard'
+    flask_version = flask.__version__
+    return 'Flask %s - GoogleAppEngine(GAE) Standard - v1.2' % flask_version
 
 
 if __name__ == '__main__':
